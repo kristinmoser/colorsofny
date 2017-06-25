f7ff8cad6c41f5acc3fb1debd4df9fef0413f1ce
Kristin Moser <km3322@nyu.edu>
2017-06-25 07:58:22 -0400

    styling & some tables

--- getPics.py
+++ getPics.py
@@ -4,7 +4,7 @@ def getVenues(cityName):
     venuesJSON = getVenuesJSON(cityName)
 
     venues = []
-    for x in xrange(1):
+    for x in xrange(2):
 
         aVenue = venuesJSON[x]['venue']
         venueName = aVenue['name']
